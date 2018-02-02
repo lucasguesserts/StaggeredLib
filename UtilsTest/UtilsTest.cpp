f5ec33d1b6f9ead22042264c750c0674eeb6c253
Lucas Guesser Targino da Silva <guesser@sinmec.ufsc.br>
2018-02-02 15:49:51 -0200

    Added Eigen as submodule. Eigen tested in UtilsTest.

--- UtilsTest/UtilsTest.cpp
+++ UtilsTest/UtilsTest.cpp
@@ -1,7 +1,10 @@
 #include <Utils/Test.h>
 #include <vector>
 
-TestCase( "catch test", "[basic]" )
+#include <Eigen/Core>
+#include <Eigen/LU>
+
+TestCase("catch test", "[basic]")
 {
 	int a,b;
 	section("true test")
@@ -30,7 +33,8 @@ TestCase("catch floating point test", "[basic]")
 	require(a==Approx(b));
 }
 
-TestCase( "cpp_11_foreach", "[basic]" ){
+TestCase("cpp_11_foreach", "[basic]")
+{
 	std::vector<double> values;
 	int nValues = 5;
 	double rawValues[] = { 2.71, 3.14, 0.0, -3.14, -2.71 };
@@ -44,4 +48,38 @@ TestCase( "cpp_11_foreach", "[basic]" ){
 		check(rawValues[counter]==v);
 		++counter; 
 	}
+}
+
+TestCase("eigen vector", "[eigen]")
+{
+	const unsigned size = 2;
+	unsigned value[size] = {0,1};
+	Eigen::VectorXd vector;
+	vector.resize(size);
+	for(unsigned i=0 ; i<size ; ++i)
+		vector[i] = value[i];
+	for(unsigned i=0 ; i<size ; ++i)
+		check(vector[i]==value[i]);
+}
+
+TestCase("eigen vector 3D", "[eigen]")
+{
+	Eigen::Vector3d vector(1,2,3);
+	for(unsigned i=0 ; i<3 ; ++i)
+		check(vector[i]==(i+1));
+}
+
+TestCase("Matrix solver", "[eigen]")
+{
+	const unsigned size = 2;
+	Eigen::MatrixXd matrix;
+		matrix.resize(size, size);
+		matrix << 2, -1, -1, 2;
+	Eigen::VectorXd independent;
+		independent.resize(size);
+		independent << 0, 12;
+	Eigen::VectorXd solution;
+		solution.resize(size);
+		solution << 4, 8;
+	check(solution==matrix.fullPivLu().solve(independent));
 }
