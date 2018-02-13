d72d144f72e838c6a56cac055e9edfdba9261628
Lucas Guesser Targino da Silva <guesser@sinmec.ufsc.br>
2018-02-13 10:25:48 -0200

    GridBuilder code cleaned.

--- Grid/GridBuilder.cpp
+++ Grid/GridBuilder.cpp
@@ -4,9 +4,24 @@
 Grid2D GridBuilder::build2D(const GridData gridData)
 {
 	Grid2D grid2D;
-	// Dimension
-	grid2D.dimension = gridData.dimension;
-	// Vertices
+	if(GridBuilder::gridIs2D(gridData))
+	{
+		grid2D.dimension = 2;
+		GridBuilder::buildVerticesOn2DGrid(gridData, grid2D);
+		GridBuilder::buildTrianglesOn2DGrid(gridData, grid2D);
+		GridBuilder::buildQuadranglesOn2DGrid(gridData, grid2D);
+		GridBuilder::assignElementsPointers(grid2D);
+	}
+	return grid2D;
+}
+
+bool GridBuilder::gridIs2D(const GridData& gridData)
+{
+	return gridData.dimension==2;
+}
+
+void GridBuilder::buildVerticesOn2DGrid(const GridData& gridData, Grid2D& grid2D)
+{
 	const unsigned numberOfVertices = gridData.coordinates.rows();
 	grid2D.vertices.reserve(numberOfVertices);
 	for(unsigned vertexIndex=0 ; vertexIndex<numberOfVertices ; ++vertexIndex)
@@ -16,36 +31,50 @@ Grid2D GridBuilder::build2D(const GridData gridData)
 		const double z = gridData.coordinates(vertexIndex,2);
 		grid2D.vertices.push_back(Vertex(x, y, z, vertexIndex));
 	}
-	// Triangles
+	return ;
+}
+
+void GridBuilder::buildTrianglesOn2DGrid(const GridData& gridData, Grid2D& grid2D)
+{
+	const unsigned numberOfVerticesPerTriangle = 3;
 	const unsigned numberOfTriangles = gridData.triangleConnectivity.rows();
 	grid2D.triangles.reserve(numberOfTriangles);
 	for(unsigned triangleIndex=0 ; triangleIndex<numberOfTriangles ; ++triangleIndex)
 	{
 		Triangle triangle;
-		for(unsigned vertexIndexInTriangle=0 ; vertexIndexInTriangle<3 ; ++vertexIndexInTriangle)
+		for(unsigned vertexLocalIndexInTriangle=0 ; vertexLocalIndexInTriangle<numberOfVerticesPerTriangle ; ++vertexLocalIndexInTriangle)
 		{
-			unsigned vertexIndex = gridData.triangleConnectivity(triangleIndex,vertexIndexInTriangle);
+			unsigned vertexIndex = gridData.triangleConnectivity(triangleIndex,vertexLocalIndexInTriangle);
 			triangle.vertices.push_back(&(grid2D.vertices[vertexIndex]));
 		}
 		grid2D.triangles.push_back(triangle);
 	}
-	// Quadrangles
+	return ;
+}
+
+void GridBuilder::buildQuadranglesOn2DGrid(const GridData& gridData, Grid2D& grid2D)
+{
+	const unsigned numberOfVerticesPerQuadrangle = 4;
 	const unsigned numberOfQuadrangles = gridData.quadrangleConnectivity.rows();
 	grid2D.quadrangles.reserve(numberOfQuadrangles);
 	for(unsigned quadrangleIndex=0 ; quadrangleIndex<numberOfQuadrangles ; ++quadrangleIndex)
 	{
 		Quadrangle quadrangle;
-		for(unsigned vertexIndexInQuadrangle=0 ; vertexIndexInQuadrangle<4 ; ++vertexIndexInQuadrangle)
+		for(unsigned vertexLocalIndexInQuadrangle=0 ; vertexLocalIndexInQuadrangle<numberOfVerticesPerQuadrangle ; ++vertexLocalIndexInQuadrangle)
 		{
-			unsigned vertexIndex = gridData.quadrangleConnectivity(quadrangleIndex,vertexIndexInQuadrangle);
+			unsigned vertexIndex = gridData.quadrangleConnectivity(quadrangleIndex,vertexLocalIndexInQuadrangle);
 			quadrangle.vertices.push_back(&(grid2D.vertices[vertexIndex]));
 		}
 		grid2D.quadrangles.push_back(quadrangle);
 	}
-	// Elements
+	return ;
+}
+
+void GridBuilder::assignElementsPointers(Grid2D& grid2D)
+{
 	for(Triangle& triangle: grid2D.triangles)
 		grid2D.elements.push_back((Element*) &triangle);
 	for(Quadrangle& quadrangle: grid2D.quadrangles)
 		grid2D.elements.push_back((Element*) &quadrangle);
-	return grid2D;
+	return ;
 }
--- include/Grid/GridBuilder.hpp
+++ include/Grid/GridBuilder.hpp
@@ -8,6 +8,12 @@ class GridBuilder: public Grid
 {
 	public:
 		static Grid2D build2D(const GridData gridData);
+	private:
+		static bool gridIs2D(const GridData& gridData);
+		static void buildVerticesOn2DGrid(const GridData& gridData, Grid2D& grid2D);
+		static void buildTrianglesOn2DGrid(const GridData& gridData, Grid2D& grid2D);
+		static void buildQuadranglesOn2DGrid(const GridData& gridData, Grid2D& grid2D);
+		static void assignElementsPointers(Grid2D& grid2D);
 };
 
 #endif
