7c54cff303e9fee073d1ebb5308976b3aebd184e
Lucas Guesser Targino da Silva <guesser@sinmec.ufsc.br>
2018-04-05 18:13:00 -0300

    VectorStencil print cleaned.

--- Stencil/VectorStencil.cpp
+++ Stencil/VectorStencil.cpp
@@ -74,3 +74,33 @@ bool operator==(const VectorStencil& lhs, const VectorStencil& rhs)
 		vality = false;
 	return vality;
 }
+
+std::string vectorStencilPairToString(const std::string& initialChars, const std::pair<unsigned,Eigen::Vector3d>& pair, const std::string& finalChars)
+{
+	// numberString: "+1.1234567890e+123, " -> 18+2 char
+	// row format: " -2.1234567890e+385, +8.9437813657e-028,\n" -> initialChars = " ", finalChars = ",\n"
+	return initialChars +
+	       "{" +
+	       std::to_string(pair.first) +
+	       "," +
+	       eigenVector3dToString(pair.second) +
+	       "}" +
+	       finalChars;
+}
+
+std::string vectorStencilToString(const VectorStencil& vectorStencil)
+{
+	// Format:
+	// {{1, [-1.27e-3,+4.19e+53,-2.83e-01]},
+	// {{4, [-1.27e-3,+4.19e+53,-2.83e-01]}}
+	std::string str;
+	str += "{";
+	VectorStencil::const_iterator it = vectorStencil.cbegin();
+		str += vectorStencilPairToString("", *it, ",\n");
+	for(it=(++vectorStencil.cbegin()) ; it!=(--vectorStencil.cend()) ; ++it)
+		str += vectorStencilPairToString(" ", *it, ",\n");
+	it = --vectorStencil.cend();
+		str += vectorStencilPairToString(" ", *it, "");
+	str += "}";
+	return str;
+}
--- Stencil/VectorStencil.hpp
+++ Stencil/VectorStencil.hpp
@@ -5,7 +5,7 @@
 #include <Eigen/Core>
 #include <Stencil/ScalarStencil.hpp>
 #include <string>
-#include <cstring>
+#include <Utils/String.hpp>
 
 using VectorStencil = std::map<unsigned,Eigen::Vector3d>;
 
@@ -16,28 +16,16 @@ VectorStencil operator*(const double scalar, const VectorStencil& vectorStencil)
 Eigen::Vector3d operator*(const VectorStencil& vectorStencil, const Eigen::VectorXd& scalarField);
 
 bool operator==(const VectorStencil& lhs, const VectorStencil& rhs);
+std::string vectorStencilToString(const VectorStencil& vectorStencil);
 
-namespace Catch{
+namespace Catch
+{
 	template<>
-	struct StringMaker<VectorStencil>{
-		static std::string convert( VectorStencil const& vectorStencil ){
-            constexpr int pairBufSize = 3*(7+7+18);
-			int bufSize = 15 + pairBufSize*vectorStencil.size();
-			char* buf = new char[bufSize]; std::strcpy(buf,"VectorStencil{");
-            char pairBuf[pairBufSize];
-			for(auto& keyValuePair: vectorStencil)
-            {
-				std::sprintf(pairBuf, "{%u,[%+10.10le,%+10.10le,%+10.10le]},",
-					keyValuePair.first,
-					keyValuePair.second.coeff(0),
-					keyValuePair.second.coeff(1),
-					keyValuePair.second.coeff(2));
-                std::strcat(buf,pairBuf);
-            }
-            std::strcat(buf,"}");
-            std::string message(buf);
-            delete[] buf;
-			return message;
+	struct StringMaker<VectorStencil>
+	{
+		static std::string convert( VectorStencil const& vectorStencil )
+		{
+			return vectorStencilToString(vectorStencil);
 		}
 	};
 }
