3dd3abc816a718b544ed31c0e77e29111f508e01
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2018-04-30 11:14:09 -0300

    Face created. Test not done.
    Consider removing the vertex reference and use
    the local index to get that information.

---
+++ GeometricEntity/Face.cpp
@@ -0,0 +1,22 @@
+#include <GeometricEntity/Face.hpp>
+
+Face::Face(
+	const unsigned localIndex,
+	Element* parentElement,
+	Vertex* adjacentVertex,
+	StaggeredElement* backwardStaggeredElement,
+	StaggeredElement* forwardStaggeredElement)
+	: localIndex(localIndex),
+	  parentElement(parentElement),
+	  adjacentVertex(adjacentVertex),
+	  backwardStaggeredElement(backwardStaggeredElement),
+	  forwardStaggeredElement(forwardStaggeredElement)
+	{}
+
+Eigen::Vector3d Face::getAreaVector(void)
+{
+	Eigen::Vector3d areaVector = *(this->adjacentVertex) - this->parentElement->getCentroid();
+	std::swap(areaVector[0],areaVector[1]);
+	areaVector[1] = - areaVector[1];
+	return areaVector;
+}
