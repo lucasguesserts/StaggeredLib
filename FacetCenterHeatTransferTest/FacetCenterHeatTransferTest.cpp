c44313ae62b5f953a5e6f015b01aafa55f222f77
Lucas Guesser Targino da Silva <guesser@sinmec.ufsc.br>
2018-05-11 13:02:42 -0300

    facet center heat transfer compute gradient matrix tested.

--- FacetCenterHeatTransferTest/FacetCenterHeatTransferTest.cpp
+++ FacetCenterHeatTransferTest/FacetCenterHeatTransferTest.cpp
@@ -8,29 +8,29 @@ TestCase("Gradient matrices")
 	const std::string cgnsGridFileName = CGNSFile::gridDirectory + "two_triangles.cgns";
 	CGNSFile cgnsFile(cgnsGridFileName);
 	GridData gridData(cgnsFile);
-	// FacetCenterHeatTransfer problem(gridData);
-	// section("triangle 0")
-	// {
-	// 	Eigen::MatrixXd gradientMatrix(3,3);
-	// 	gradientMatrix << -0.50000,  0.50000, 0.00000,
-	// 	                   0.00000, -0.50000, 0.50000,
-	// 	                   0.00000,  0.00000, 0.00000;
-	// 	for(unsigned faceIndex=0 ; faceIndex<3 ; ++faceIndex)
-	// 	{
-	// 		Face2D& face = problem.grid2D.faces[faceIndex];
-	// 		check(problem.computeGradientMatrix(face)==gradientMatrix);
-	// 	}
-	// }
-	// section("triangle 0")
-	// {
-	// 	Eigen::MatrixXd gradientMatrix(3,3);
-	// 	gradientMatrix <<  0.00000, 0.50000, -0.50000,
-	// 	                  -0.50000, 0.00000,  0.50000,
-	// 	                   0.00000, 0.00000,  0.00000;
-	// 	for(unsigned faceIndex=3 ; faceIndex<6 ; ++faceIndex)
-	// 	{
-	// 		Face2D& face = problem.grid2D.faces[faceIndex];
-	// 		check(problem.computeGradientMatrix(face)==gradientMatrix);
-	// 	}
-	// }
+	FacetCenterHeatTransfer problem(gridData);
+	section("triangle 0")
+	{
+		Eigen::MatrixXd gradientMatrix(3,3);
+		gradientMatrix << -0.5,  0.5, 0.0,
+		                   0.0, -0.5, 0.5,
+		                   0.0,  0.0, 0.0;
+		for(unsigned faceIndex=0 ; faceIndex<3 ; ++faceIndex)
+		{
+			Face2D& face = problem.grid2D.faces[faceIndex];
+			check(problem.computeGradientMatrix(face)==gradientMatrix);
+		}
+	}
+	section("triangle 0")
+	{
+		Eigen::MatrixXd gradientMatrix(3,3);
+		gradientMatrix <<  0.0, 0.5, -0.5,
+		                  -0.5, 0.0,  0.5,
+		                   0.0, 0.0,  0.0;
+		for(unsigned faceIndex=3 ; faceIndex<6 ; ++faceIndex)
+		{
+			Face2D& face = problem.grid2D.faces[faceIndex];
+			check(problem.computeGradientMatrix(face)==gradientMatrix);
+		}
+	}
 }
