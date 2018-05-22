d72bf815dca9b6293933a09e325073d4e8b6c3d2
Lucas Guesser Targino da Silva <guesser@sinmec.ufsc.br>
2018-05-22 17:56:07 -0300

    Test of grid 2D with staggered elements export done.

--- GridTest/Grid2DWithStaggeredElementsExportTest.cpp
+++ GridTest/Grid2DWithStaggeredElementsExportTest.cpp
@@ -8,5 +8,44 @@ TestCase("Export staggered grid", "[Grid2DWithStaggeredElementsExport]")
 	const std::string testFileName = gridDirectory + ".grid2DWithStaggeredElementExport_Test.cgns";
 	Grid2DWithStaggeredElements grid(cgnsGridFileName);
 	Grid2DWithStaggeredElementsExport::cgns(testFileName, grid);
+	Grid2D testGrid(testFileName);
+	section("vertices")
+	{
+		std::vector<Vertex> vertices;
+		vertices.push_back(Vertex(0.0, 0.0, 0.0, 0));
+		vertices.push_back(Vertex(2.0, 0.0, 0.0, 1));
+		vertices.push_back(Vertex(0.0, 2.0, 0.0, 2));
+		vertices.push_back(Vertex(2.0, 2.0, 0.0, 3));
+		vertices.push_back(Vertex(4.0/3.0, 2.0/3.0, 0.0, 4));
+		vertices.push_back(Vertex(2.0/3.0, 4.0/3.0, 0.0, 5));
+		check(testGrid.vertices==vertices);
+	}
+	section("quadrangle")
+	{
+		check(testGrid.quadrangles[0].vertices[0]==&(testGrid.vertices[0]));
+		check(testGrid.quadrangles[0].vertices[1]==&(testGrid.vertices[4]));
+		check(testGrid.quadrangles[0].vertices[2]==&(testGrid.vertices[3]));
+		check(testGrid.quadrangles[0].vertices[3]==&(testGrid.vertices[5]));
+	}
+	section("triangles")
+	{
+		require(testGrid.triangles.size()==4);
+		// 0
+		check(testGrid.triangles[0].vertices[0]==&(testGrid.vertices[1]));
+		check(testGrid.triangles[0].vertices[1]==&(testGrid.vertices[4]));
+		check(testGrid.triangles[0].vertices[2]==&(testGrid.vertices[0]));
+		// 1
+		check(testGrid.triangles[1].vertices[0]==&(testGrid.vertices[3]));
+		check(testGrid.triangles[1].vertices[1]==&(testGrid.vertices[4]));
+		check(testGrid.triangles[1].vertices[2]==&(testGrid.vertices[1]));
+		// 2
+		check(testGrid.triangles[2].vertices[0]==&(testGrid.vertices[2]));
+		check(testGrid.triangles[2].vertices[1]==&(testGrid.vertices[5]));
+		check(testGrid.triangles[2].vertices[2]==&(testGrid.vertices[3]));
+		// 3
+		check(testGrid.triangles[3].vertices[0]==&(testGrid.vertices[0]));
+		check(testGrid.triangles[3].vertices[1]==&(testGrid.vertices[5]));
+		check(testGrid.triangles[3].vertices[2]==&(testGrid.vertices[2]));
+	}
 	boost::filesystem::remove_all(testFileName);
 }
