8dc7dc52dff67a6efd0d82a368e2cd2a6e6fdc0d
Lucas Guesser Targino da Silva <guesser@sinmec.ufsc.br>
2018-05-24 14:22:39 -0300

    Added gmsh grids to peters gradient test.

--- PetersFacetCenterHeatTransferTest/CompareNumericalAndAnalyticalSolutions.cpp
+++ PetersFacetCenterHeatTransferTest/CompareNumericalAndAnalyticalSolutions.cpp
@@ -23,7 +23,7 @@
 #include <CgnsInterface/CgnsWriter.hpp>
 #include <CgnsInterface/CgnsReader/CgnsReader2D.hpp>
 
-TestCase("Facet center method - compare numerical and analytical solution", "[PetersFacetCenterHeatTransfer]")
+TestCase("Facet center method - compare numerical and analytical solution - regular grids", "[PetersFacetCenterHeatTransfer]")
 {
 	const std::string directory = gridDirectory + std::string("SquareCavityHeatTransfer_AnalyticalTest/");
 	const std::string outputDirectory = gridDirectory + std::string("output/");
@@ -86,4 +86,67 @@ TestCase("Facet center method - compare numerical and analytical solution", "[Pe
 	RateOfConvergence rateOfConvergence(numericalError,characteristicLength);
 	check(rateOfConvergence.converge());
 	check(rateOfConvergence.order>0.2);
+}
+
+TestCase("Facet center method - compare numerical and analytical solution - irregular grids", "[PetersFacetCenterHeatTransfer]")
+{
+	const std::string directory = gridDirectory + std::string("gmsh/");
+	const std::string outputDirectory = gridDirectory + std::string("output/");
+	const std::vector<std::string> meshFiles = {
+		directory + "01.cgns",
+		directory + "02.cgns",
+		directory + "05.cgns"
+	};
+	const std::vector<std::string> resultFiles = {
+		outputDirectory + "facet_center_with_peters_gradient_gmsh_1.cgns",
+		outputDirectory + "facet_center_with_peters_gradient_gmsh_2.cgns",
+		outputDirectory + "facet_center_with_peters_gradient_gmsh_5.cgns"
+	};
+	std::vector<double> numericalError, characteristicLength;
+	for(unsigned count=0 ; count<meshFiles.size() ; ++count)
+	{
+		std::string cgnsFileName = meshFiles[count];
+		std::string resultFileName = resultFiles[count];
+		PetersFacetCenterHeatTransfer problem(cgnsFileName);
+
+		problem.insertDirichletBoundaryCondition("South",0.0);
+		problem.insertDirichletBoundaryCondition("East",0.0);
+		problem.insertDirichletBoundaryCondition("West",0.0);
+		problem.insertDirichletBoundaryCondition("North",
+			[](Eigen::Vector3d centroid) -> double
+			{ return std::sin(M_PI*centroid.x()) * std::sinh(M_PI*centroid.y()) / std::sinh(M_PI); });
+
+		// Steady numerical solution
+		problem.addDiffusiveTerm();
+		problem.applyBoundaryConditions();
+		Eigen::VectorXd numericalSolution = problem.linearSystem.solve();
+
+		// Analytical
+		Eigen::Matrix<double,Eigen::Dynamic,3> staggeredElementsCentroid;
+		staggeredElementsCentroid.resize(problem.grid2D.staggeredElements.size(), Eigen::NoChange);
+		for(auto staggeredElement: problem.grid2D.staggeredElements)
+		{
+			Eigen::Vector3d centroid = staggeredElement.getCentroid();
+			staggeredElementsCentroid(staggeredElement.getIndex(),0) = centroid(0);
+			staggeredElementsCentroid(staggeredElement.getIndex(),1) = centroid(1);
+			staggeredElementsCentroid(staggeredElement.getIndex(),2) = centroid(2);
+		}
+		Eigen::VectorXd analyticalSolution = SquareCavityHeatTransfer::computeAnalyticalSolution(staggeredElementsCentroid);
+
+		Eigen::VectorXd difference = numericalSolution - analyticalSolution;
+		double error = difference.lpNorm<Eigen::Infinity>();
+		numericalError.push_back(error);
+		characteristicLength.push_back(problem.grid2D.getStaggeredCharacteristicLength());
+
+		// Export
+		Grid2DWithStaggeredElementsExport::cgns(resultFileName, problem.grid2D);
+		CgnsWriter cgnsWriter(resultFileName, "CellCenter");
+		cgnsWriter.writePermanentSolution("steadySolution");
+		cgnsWriter.writePermanentField("Numerical_Temperature", std::vector<double>(&numericalSolution[0], &numericalSolution[0] + numericalSolution.size()));
+		cgnsWriter.writePermanentField("Analytical_Temperature", std::vector<double>(&analyticalSolution[0], &analyticalSolution[0] + analyticalSolution.size()));
+		cgnsWriter.writePermanentField("error", std::vector<double>(&difference[0], &difference[0] + difference.size()));
+	}
+	RateOfConvergence rateOfConvergence(numericalError,characteristicLength);
+	check(rateOfConvergence.converge());
+	check(rateOfConvergence.order>1.0);
 }
