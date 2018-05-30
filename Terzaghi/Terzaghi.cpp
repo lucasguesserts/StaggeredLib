25a078e5940c81c3b10cc997e946cc774a23d051
Lucas Guesser Targino da Silva <guesser@sinmec.ufsc.br>
2018-05-30 07:40:24 -0300

    Terzaghi set linear system size added.

--- Terzaghi/Terzaghi.cpp
+++ Terzaghi/Terzaghi.cpp
@@ -2,4 +2,8 @@
 
 Terzaghi::Terzaghi(const std::string& gridFile)
 	: grid(gridFile)
-{}
+{
+	const unsigned linearSystemSize = this->grid.elements.size() + 3 * this->grid.staggeredElements.size();
+	this->linearSystem.setSize(linearSystemSize);
+	return;
+}
--- TerzaghiTest/ConstructorTest.cpp
+++ TerzaghiTest/ConstructorTest.cpp
@@ -6,6 +6,16 @@ TestCase("Terzaghi constructor test", "[Terzaghi]")
 {
 	const std::string gridFile = gridDirectory + "GridReaderTest_CGNS.cgns";
 	Terzaghi terzaghi(gridFile);
-	terzaghi.viscosity = 1E-5;
-	check(terzaghi.viscosity==1E-5);
+	section("sizes")
+	{
+		constexpr unsigned linearSystemSize = 48;
+		check(terzaghi.linearSystem.matrix.rows()==linearSystemSize);
+		check(terzaghi.linearSystem.matrix.cols()==linearSystemSize);
+		check(terzaghi.linearSystem.independent.size()==linearSystemSize);
+	}
+	section("phisical quantities")
+	{
+		terzaghi.viscosity = 1E-5;
+		check(terzaghi.viscosity==1E-5);
+	}
 }
