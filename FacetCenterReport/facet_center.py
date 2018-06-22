b88c058730b0ec1662a5b5c50e4d3a854b36736b
Lucas Guesser Targino da Silva <guesser@sinmec.ufsc.br>
2018-06-22 07:23:52 -0300

    Save figure added.

--- FacetCenterReport/facet_center.py
+++ FacetCenterReport/facet_center.py
@@ -24,7 +24,7 @@ for i in range(5):
 	elementPlot.append(readLine(fileElement))
 plotar = [facetPlot, elementPlot]
 
-fig, ax = plt.subplots(1,2, figsize=(8,16))
+fig, ax = plt.subplots(1,2, figsize=(16,8))
 # plt.subplots_adjust(left=0.175, right=0.98, top=0.96, bottom=0.135, wspace=0.36)
 
 plt.rcParams['font.family'] = 'serif'
@@ -37,7 +37,7 @@ for j in range(len(ax)):
 	for i in range(5):
 		ax[j].loglog(plotar[j][i][0], plotar[j][i][1], linestyle='-', marker='o', color=colors[i], linewidth='1.0', label=labels[i])
 	ax[j].set_title(titles[j])
-	ax[j].legend()
+	ax[j].legend(loc='upper left')
 	ax[j].grid(True)
 	ax[j].set_xlabel('Comprimento característico', size=14)
 	ax[j].set_ylabel('Erro', size=14)
@@ -46,4 +46,5 @@ for j in range(len(ax)):
 	ax[j].grid(which='minor', linestyle=':')
 	ax[j].set_xlim([1E-2, 1])
 	ax[j].set_ylim([1E-4, 1E-1])
-plt.show()
+# plt.show()
+fig.savefig('/home/guesser/report_heat_transfer.pdf')
